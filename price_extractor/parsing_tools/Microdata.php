85c3248584a33e845c6c5d03783613e17fd9169d
Dmitry <lbvf50.mobile@gmail.com>
2019-11-05 20:59:35 +0700

    Solve complicate problem about getting item only by attribute

--- price_extractor/parsing_tools/Microdata.php
+++ price_extractor/parsing_tools/Microdata.php
@@ -12,10 +12,16 @@ class Microdata{
     }
     function test(){
         $this->extractNameAndPrice();
+        $this->check_for_product();
         return false;
     }
     function extractNameAndPrice(){
         $this->name = "This is Microdata";
         $this->price = 10000;
+    }
+    protected function check_for_product(){
+        $query = "count(//*[@itemtype='http://schema.org/Product'])";
+        $entries = $this->xpath->evaluate($query, $this->dom);
+        echo "There are $entries products\n";
     }
 }

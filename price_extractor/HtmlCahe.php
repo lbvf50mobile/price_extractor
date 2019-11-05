e02a84c1a3060fe03b00749aef6d22a35f38e8c3
Dmitry <lbvf50.mobile@gmail.com>
2019-11-05 15:45:32 +0700

    Before testing caching

--- price_extractor/HtmlCahe.php
+++ price_extractor/HtmlCahe.php
@@ -15,11 +15,9 @@ class HtmlCache implements \IteratorAggregate{
         }
     }
     function fill_results(){
-        $this->results = [
-            new CachedElemet("httpone"),
-            new CachedElemet("two"),
-            new CachedElemet("httpthree"),
-        ];
+        foreach($this->sources_list as $source){
+            array_push($this->results, new CachedElemet($source));
+        }
     }
     function getIterator(){
         return new \ArrayIterator($this->results);
@@ -27,10 +25,14 @@ class HtmlCache implements \IteratorAggregate{
 }
 
 class CachedElemet {
-    public  $html;
-    public  $source;
+    public $html;
+    public $source;
     public $type;
+    public $cache_file_name;
+
     private $is_url;
+    private $cache_dir;
+    
 
     function __construct($resource_path){
         $this->cache_dir = dirname(__FILE__)."/cache";
@@ -39,8 +41,9 @@ class CachedElemet {
         $this->setHtml();
        
     }
+
     protected function setHtml(){
-        $this->html = "this is html";
+        $this->html = $this->getHtml();
     }
     protected function setType(){
         $this->defineUrlOrFile();
@@ -48,5 +51,25 @@ class CachedElemet {
     }
     protected function defineUrlOrFile(){
         return $this->is_url = (0 == preg_match('/^http/',$this->source)) ? false : true;
+    }
+    protected function defineCacheFileName(){
+        $this->cache_file_name = $this->is_url ? md5($this->source) : $this->source;
+        $this->cache_file_name = $this->cache_dir.'/'.$this->cache_file_name;
+    }
+    protected function getHtml(){
+        if(file_exists($this->cache_file_name)){
+            return file_get_contents($this->cache_file_name);
+        }else{
+            return $this->createCacheForUncachedFile();
+        }
+    }
+    protected function createCacheForUncachedFile(){
+        if($this->is_url){
+            $html = file_get_contents($this->source);
+            file_put_contents($this->cache_file_name, $html);
+            return $html;
+        }else{
+            throw new Exception('Unexisted file: '.$this->source);
+        }
     }
 }
